287a5415d15432ce4581fe1c3a0bbb93fc8c8ac7
Chavela <angie87wong@gmail.com>
2024-11-06 09:02:42 -0300

    Configuro mysqlConfig.js

--- backend/config/mysqlConfig.js
+++ backend/config/mysqlConfig.js
@@ -1,1 +1,8 @@
 /**Aquí va la configuración para conectarse a MySQL */
+
+module.exports = {
+    host: 'localhost',
+    user: 'root',
+    password: 'tu_password',
+    database: 'nombre_de_la_base'
+  };
